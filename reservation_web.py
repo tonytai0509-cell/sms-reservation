ee3e02a269fdca1b9063ac273de4c33ef3a9e006
tonytai0509-cell <tony.tai0509@gmail.com>
2026-07-25 00:16:58 +0200

    Update reservation_web.py

--- reservation_web.py
+++ reservation_web.py
@@ -772,10 +772,12 @@ FORMULAIRE_RESERVATION_HTML = """
   </form>
 
   <div class="pied">
+    {% if not mode_admin %}
     <div class="ligne">
       <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/></svg>
       Vous recevrez une confirmation par SMS
     </div>
+    {% endif %}
     <div class="ligne">
       <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6A19.79 19.79 0 0 1 2.12 4.18 2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.12.81.3 1.6.54 2.37a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.71-1.11a2 2 0 0 1 2.11-.45c.77.24 1.56.42 2.37.54A2 2 0 0 1 22 16.92z"/></svg>
       Besoin d'aide ? <a href="tel:+33624836448">Appelez la centrale</a>
